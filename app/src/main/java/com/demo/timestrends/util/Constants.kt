c350162aa148dc74f58b52f2ea1f75ecf4682106
Jade Byfield <jade@thoughtbot.com>
2023-06-07 21:21:52 -0400

    Remove unneccessary reference

--- app/src/main/java/com/demo/timestrends/util/Constants.kt
+++ app/src/main/java/com/demo/timestrends/util/Constants.kt
@@ -1,5 +1,5 @@
 package com.demo.timestrends.util;
 
-  const val API_KEY = "Y4t9s5OydmvKj5WpkRQODCpXtruB9lI8"
-  const val BASE_URL = "https://api.nytimes.com/"
+
+const val BASE_URL = "https://api.nytimes.com/"
 
